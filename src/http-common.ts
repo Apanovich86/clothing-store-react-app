b496a7bc96adf16d126e2f4de6cf0b4490777b69
Apanovich86 <apanovich.anna.itec@gmail.com>
2022-09-28 09:38:00 +0200

    add config to front

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8092/api",
+    baseURL: "https://java-postgresql-internet-store.herokuapp.com/api",
     headers: {
         "Content-type": "application/json"
     }
